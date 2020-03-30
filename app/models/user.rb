6e659eb1a7063da37d590d8a44286dfd76f8cbef
yusuke3939 <y.t.041990@gmail.com>
2020-03-30 19:31:11 +0900

    message

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
-  
+
+  has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
 end
