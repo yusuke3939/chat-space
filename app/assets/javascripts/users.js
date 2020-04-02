8676b1ee1cf43d3392f6f0dd719d7b0d703c4710
yusuke3939 <y.t.041990@gmail.com>
2020-04-02 18:11:23 +0900

    ajax add

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,33 @@
 $(function(){
 
-$("").on('keyup' function(){
+  let serch_list = $("#user-search-result");
 
-}
+  function
 
 
+  $("#user-search-field").on("keyup", function(){
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      dataType: 'json',
+      data: { keyword: input }
+    })
+    .done(function(users){
+      search_list.empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+        appendUser(user);
+        });
+      } 
+      else {
 
-})
+      }
+      
+
+    })
+    .fail(function(){
+      console.log("失敗です");
+    });
+  });
+});
