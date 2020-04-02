c6ca5b2cb28f59ab86647cefd52e1b8e997933b0
yusuke3939 <y.t.041990@gmail.com>
2020-04-02 19:38:16 +0900

    alert add

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,9 +1,18 @@
 $(function(){
+  function addUser(user) {
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    $("#user-search-result").append(html);
+  }
 
-  let serch_list = $("#user-search-result");
-
-  function
-
+  function addNoUser() {
+    let html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">ユーザーが見つかりません</p>
+                </div>`
+    $("#user-search-result").append(html);
+  }
 
   $("#user-search-field").on("keyup", function(){
     let input = $("#user-search-field").val();
@@ -13,21 +22,21 @@ $(function(){
       dataType: 'json',
       data: { keyword: input }
     })
-    .done(function(users){
-      search_list.empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-        appendUser(user);
-        });
-      } 
-      else {
+      .done(function(users){
+        $("#user-search-result").empty();
 
-      }
-      
-
-    })
-    .fail(function(){
-      console.log("失敗です");
-    });
+        if (users.length !== 0) {
+          users.forEach(function(user){
+          let html = addUser(user);
+          });
+        } else if (input.length == 0 ) {
+          return false;
+        } else {
+          addNoUser();
+        }
+      })
+      .fail(function() {
+        alert("通信エラーです。ユーザーが表示されません。")
+      });
   });
 });
