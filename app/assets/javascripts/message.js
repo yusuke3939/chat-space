834b40cc1b5a47c2b069ebbe9c806fbd766c375f
yusuke3939 <y.t.041990@gmail.com>
2020-04-03 12:01:31 +0900

    add message reload

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,4 +63,20 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      console.log('success');
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
 })
